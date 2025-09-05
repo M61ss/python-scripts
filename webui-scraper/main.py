185420ee68897f158fb4da8ca6e448ec9e2673af
Mattia Massarenti <mattia.mas2003@gmail.com>
2025-09-05 11:37:12 +0200

    rearrange code for a more realistic usage in webui

--- webui-scraper/main.py
+++ webui-scraper/main.py
@@ -4,47 +4,47 @@ from query import Query
 import sys
 
 class App:
-    def __init__(self):
-        self.debug = True if "--debug" in sys.argv else False
-        self.queries = []
+    def __init__(self, nlp : NLP, debug : bool = False):
+        self.nlp = nlp
+        self.debug = debug
 
-    def retrive_scraping(self):
+    def __retrive_scraping(self, query : Query):
         scraper = Scraper(self.debug)
         outputs = []
-        for query in self.queries:
-            query_output = scraper.scrape(query)
-            if self.debug:
-                print(f"Output for query {query}:\n{query_output}")
-            outputs.append(query_output)
-
-    def submit_queries(self, queries : Query):
-        self.queries.extend(queries)
+        query_output = scraper.scrape(query)
         if self.debug:
-            for i, query in enumerate(self.queries):
-                print(f"Query number {i}:")
-                print(f"    - Content: {query.content}")
-                print(f"    - Language: {query.lang}")
-                print(f"    - Keywords: {query.keywords}")
-            print("")
-            print(f"Total number of submitted queries: {len(self.queries)}")
+            print(f"Output for query {query.content}:\n{query_output}")
+        outputs.append(query_output)
+        return outputs
 
+    def submit_queries(self, query_content : str):
+        query : Query = self.nlp.compose_query(query_content)
+        if self.debug:
+            print(f"Query information:")
+            print(f"    - Content: {query.content}")
+            print(f"    - Language: {query.lang}")
+            print(f"    - Keywords: {query.keywords}")
+        return self.__retrive_scraping(query)
 
 if __name__ == "__main__":
-    app = App()
-    nlp = NLP(app.debug)
+    debug = True if "--debug" in sys.argv else False
+    
+    nlp = NLP(debug)
+    app = App(nlp, debug)
 
-    queries = [
-        nlp.compose_query("Oggi il sole splende e il cielo è sereno."),             # Italiano, IT
-        nlp.compose_query("The quick brown fox jumps over the lazy dog."),          # Inglese, EN
-        nlp.compose_query("Demain, il fera beau et nous irons au marché."),         # Francese, FR
-        nlp.compose_query("La biblioteca está cerrada durante el fin de semana."),  # Spagnolo, ES
-        nlp.compose_query("Der Winter ist kalt, aber auch sehr schön."),            # Tedesco, DE
-        nlp.compose_query("O café da manhã foi delicioso hoje."),                   # Portoghese, PT
-        nlp.compose_query("De kinderen spelen buiten in de tuin."),                 # Olandese, NL
-        nlp.compose_query("Сегодня вечером будет очень холодно."),                  # Russo, RU
-        nlp.compose_query("今天的天气非常好。"),                                      # Cinese, ZH
-        nlp.compose_query("今日は学校に行きます。"),                                   # Giapponese, JA
-        nlp.compose_query("السماء صافية اليوم والشمس مشرقة."),                    # Arabo, AR
+    query_contents = [
+        "Oggi il sole splende e il cielo è sereno.",             # Italiano, IT
+        "The quick brown fox jumps over the lazy dog.",          # Inglese, EN
+        "Demain, il fera beau et nous irons au marché.",         # Francese, FR
+        "La biblioteca está cerrada durante el fin de semana.",  # Spagnolo, ES
+        "Der Winter ist kalt, aber auch sehr schön.",            # Tedesco, DE
+        "O café da manhã foi delicioso hoje.",                   # Portoghese, PT
+        "De kinderen spelen buiten in de tuin.",                 # Olandese, NL
+        "Сегодня вечером будет очень холодно.",                  # Russo, RU
+        "今天的天气非常好。",                                      # Cinese, ZH
+        "今日は学校に行きます。",                                   # Giapponese, JA
+        "السماء صافية اليوم والشمس مشرقة.",                    # Arabo, AR
     ]
     
-    app.submit_queries(queries)
+    for query_content in query_contents:
+        app.submit_queries(query_content)
