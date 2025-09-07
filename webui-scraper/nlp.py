6f1577b4d3f55efab6e3a21f0677a53f627ac1fe
Mattia Massarenti <mattia.mas2003@gmail.com>
2025-09-07 21:18:51 +0200

    handle case of no language detection
    if no language is detected now the script is able to proceed without throwing errors. If no language is detected, it is set on english by default

--- webui-scraper/nlp.py
+++ webui-scraper/nlp.py
@@ -10,14 +10,17 @@ class NLP:
 
     def compose_query(self, content : str):
         query_lang = self.__detect_language(content)
+        if not query_lang:
+            query_lang = Language.ENGLISH.iso_code_639_1.name
         query_keywords = self.__extract_keywords(content)
         return Query(content, query_lang, query_keywords)
 
     def __detect_language(self, content : str):
         language : Language = self.detector.detect_language_of(content)
+        lang = language.iso_code_639_1.name if language else None
         if self.debug:
-            print(f"Detected language for query '{content}': {language.iso_code_639_1.name}")
-        return language.iso_code_639_1.name
+            print(f"Detected language for query '{content}': {lang}")
+        return lang
     
     def __extract_keywords(self, content : str):
         return self.kw_model.extract_keywords(content)
