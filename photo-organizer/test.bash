d5b27c3d537ace3253b2c527af7a203e30b1fea2
Mattia Massarenti <mattia.mas2003@gmail.com>
2025-10-19 19:01:19 +0200

    simplify test.bash text output

--- photo-organizer/test.bash
+++ photo-organizer/test.bash
@@ -1,28 +1,17 @@
 #!/bin/bash
 
-######################################### HELP
-
-echo "*** HELP TEST START ***"
+echo "############## HELP "##############
 printf "\n"
 
 ./main.py --help
 
-printf "\n"
-echo "*** HELP TEST END ***"
-
-######################################### FUNCTIONAL
-
-echo "*** FUNCTIONAL TEST START ***"
+echo "############## FUNCTIONAL "##############
 printf "\n"
 
+rm -rf sorted
 ./main.py test-batch sorted/
 
-printf "\n"
-echo "*** FUNCTIONAL TEST END ***"
-
-######################################### VERBOSITY
-
-echo "*** VERBOSITY TEST START ***"
+echo "############## VERBOSITY "##############
 printf "\n"
 
 echo "  1. Checking --verbose output"
@@ -36,17 +25,9 @@ printf "\n"
 rm -rf sorted
 ./main.py test-batch sorted/ --verbose --debug
 
-printf "\n"
-echo "*** VERBOSITY TEST END ***"
-
-######################################### STOP
-
-echo "*** STOP CONDITION TEST START ***"
+echo "############## STOP CONDITION TEST START "##############
 printf "\n"
 
 echo "  1. Already existing not empty folder should cause termination"
 printf "\n"
 ./main.py test-batch sorted/
-
-printf "\n"
-echo "*** STOP CONDITION TEST END ***"
