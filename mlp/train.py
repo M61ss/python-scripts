10e2614d65c2e37571402fa5a487c74766d6d155
Mattia Massarenti <mattia.mas2003@gmail.com>
2025-12-11 14:25:52 +0100

    add training script for MNIST dataset with DataLoader

---
+++ mlp/train.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+from torchvision.datasets import MNIST
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+
+BATCH_SIZE = 128
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+
+training_data = MNIST('data', 
+                      train=True, 
+                      transform=transform, 
+                      download=True
+                    )
+dl = DataLoader(dataset=training_data,
+                batch_size=BATCH_SIZE,
+                num_workers=2,
+                drop_last=True,
+                shuffle=True
+            )
