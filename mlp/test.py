1cfc803c288a858e9c5e733278b5dce960c01d6b
Mattia Massarenti <mattia.mas2003@gmail.com>
2025-12-12 19:28:01 +0100

    refactor: reorganize test.py to include device setup and model initialization

--- mlp/test.py
+++ mlp/test.py
@@ -1,8 +1,18 @@
+import torch
 from torch.utils.data import DataLoader
 from torchvision.transforms import transforms
 from torchvision.datasets import MNIST
+from mlp import MLP
 
 BATCH_SIZE = 128
+
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+NUM_EPOCHS = 10
+FAN_IN = 28 * 28
+N_CLASSES = 10
+HIDDEN_DIM = 128
+LEARNING_RATE = 0.001
+mlp = MLP(FAN_IN, HIDDEN_DIM, N_CLASSES).to(DEVICE)
 
 transform = transforms.Compose([
     transforms.ToTensor(),
@@ -22,3 +32,9 @@ dl = DataLoader(
     shuffle=True,
     drop_last=True
 )
+
+correct = 0
+total = 0
+
+for x, y in dl:
+    x, y = x.to(DEVICE), y.to(DEVICE)
