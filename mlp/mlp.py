4274dd74580506d209c12ecb6bcbcaad4f18a4b0
Mattia Massarenti <mattia.mas2003@gmail.com>
2025-12-12 19:37:47 +0100

    refactor: hardcode net specs in the MLP module

--- mlp/mlp.py
+++ mlp/mlp.py
@@ -1,18 +1,17 @@
 import torch
 import torch.nn as nn
 
-torch.cuda.is_available()
+FAN_IN = 28 * 28
+N_CLASSES = 10
+HIDDEN_DIM = 128
 
 class MLP(nn.Module):
-    """
-    Works with: 28x28 grayscale images
-    """
-    def __init__(self, input_dim : int, hidden_dim : int, output_dim : int):
+    def __init__(self):
         super(MLP, self).__init__()
 
-        self.in_dim = input_dim
-        self.h_dim = hidden_dim
-        self.out_dim = output_dim
+        self.in_dim = FAN_IN
+        self.h_dim = HIDDEN_DIM
+        self.out_dim = N_CLASSES
 
         self.net = nn.Sequential(
             nn.Linear(self.in_dim, self.h_dim),
