4f88a2a8b7b57f8a1ddfb03d346b8d69e0e1b93c
Mattia Massarenti <mattia.mas2003@gmail.com>
2026-04-05 22:13:33 +0200

    refactor

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -5,13 +5,22 @@ import torch.nn as nn
 class SelfAttention(nn.Module):
     def __init__(self, d_model: int, d_qk: int, d_v: int, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.d_model = d_model
-        self.d_qk = d_qk
-        self.d_v = d_v
+        self.d_model    = d_model
+        self.d_qk       = d_qk
+        self.d_v        = d_v
 
-        self.qW = nn.Linear(d_model, d_qk)
-        self.kW = nn.Linear(d_model, d_qk)
-        self.vW = nn.Linear(d_model, d_v)
+        self.qW = nn.Linear(
+            in_features=d_model, 
+            out_features=d_qk
+        )
+        self.kW = nn.Linear(
+            in_features=d_model, 
+            out_features=d_qk
+        )
+        self.vW = nn.Linear(
+            in_features=d_model, 
+            out_features=d_v
+        )
 
 
     def forward(self, X: torch.Tensor):
@@ -25,10 +34,10 @@ class SelfAttention(nn.Module):
 class MultiHeadAttetion(nn.Module):
     def __init__(self, num_heads: int, d_model: int, d_qk: int, d_v: int, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.num_heads = num_heads
-        self.d_model = d_model
-        self.d_qk = d_qk
-        self.d_v = d_v
+        self.num_heads  = num_heads
+        self.d_model    = d_model
+        self.d_qk       = d_qk
+        self.d_v        = d_v
 
         self.heads = nn.ModuleList(
             [SelfAttention(
@@ -46,11 +55,11 @@ class MultiHeadAttetion(nn.Module):
 class TransformerEncoderBlock(nn.Module):
     def __init__(self, num_heads: int, d_model: int, d_qk: int, d_v: int, fc_hidden_dim: int = 512, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.num_heads = num_heads
-        self.d_model = d_model
-        self.fc_hidden_dim = fc_hidden_dim
-        self.d_qk = d_qk
-        self.d_v = d_v
+        self.num_heads      = num_heads
+        self.d_model        = d_model
+        self.fc_hidden_dim  = fc_hidden_dim
+        self.d_qk           = d_qk
+        self.d_v            = d_v
 
         self.mha = MultiHeadAttetion(
             num_heads=num_heads, 
@@ -83,8 +92,8 @@ class Transformer(nn.Module):
     def __init__(self, num_blocks: int, num_heads: int, d_model: int, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.num_blocks = num_blocks
-        self.num_heads = num_heads
-        self.d_model = d_model
+        self.num_heads  = num_heads
+        self.d_model    = d_model
         
         self.net = nn.ModuleList(
             [TransformerEncoderBlock(num_heads, d_model) for _ in range(num_blocks)]
