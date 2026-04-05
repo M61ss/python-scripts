0cb5ff6d631240ba65e6f3ab8e2356dc23d38294
Mattia Massarenti <mattia.mas2003@gmail.com>
2026-04-05 23:09:57 +0200

    refactor

--- mlp/mlp.py
+++ mlp/mlp.py
@@ -6,12 +6,12 @@ class MLP(nn.Module):
     """
     Works with 28 * 28 images
     """
-    def __init__(self, fan_in: int, hidden_dim: int, n_classes: int, *args, **kwargs):
+    def __init__(self, in_dim: int, h_dim: int, out_dim: int, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.in_dim     = fan_in
-        self.h_dim      = hidden_dim
-        self.out_dim    = n_classes
+        self.in_dim     = in_dim
+        self.h_dim      = h_dim
+        self.out_dim    = out_dim
 
         self.net = nn.Sequential(
             nn.Linear(self.in_dim, self.h_dim),
