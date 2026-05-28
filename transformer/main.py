a33c0d4f54c02ce6f72ada43097741027eef1dcc
Mattia Massarenti <mattia.mas2003@gmail.com>
2026-05-28 16:57:32 +0200

    compose main

--- transformer/main.py
+++ transformer/main.py
@@ -1,2 +1,19 @@
-from encoder import TransformerEncoderBlock
+from encoder import TransformerEncoder
 
+
+N_BLOCKS = 6
+N_HEADS = 8
+D_MODEL = 128
+D_QK = 32
+D_V = 64
+FC_HIDDEN_DIM = 512
+
+
+te = TransformerEncoder(
+    n_blocks=N_BLOCKS,
+    num_heads=N_HEADS,
+    d_model=D_MODEL,
+    d_qk=D_QK,
+    d_v=D_V,
+    fc_hidden_dim=FC_HIDDEN_DIM
+)
