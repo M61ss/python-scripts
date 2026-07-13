190075075f2f5eee65243389ca95773eb62c56bf
Mattia Massarenti <mattia.mas2003@gmail.com>
2026-07-13 21:45:52 +0200

    merge main and detector

--- image-dump/detector.py
+++ image-dump/detector.py
@@ -1,5 +1,15 @@
 import exif
+import sys
 
-def collect_metadata(img_filename : str):
+def collect_metadata(img_filename: str):
     img : exif.Image = exif.Image(img_filename)
     return img.get_all()
+
+
+if len(sys.argv) == 2:
+    img_filename: str = sys.argv[1]
+    metadata: dict[str] = collect_metadata(img_filename)
+    for key, value in metadata.items():
+        print(f"{key}: {value}")
+else:
+    raise RuntimeError("Wrong number of parameter.")
