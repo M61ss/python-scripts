9071ca961988f6de8ee0ac55d1410e026cb0da7e
Mattia Massarenti <mattia.mas2003@gmail.com>
2026-09-23 21:58:48 +0200

    correct main according to changes

--- transformer/main.py
+++ transformer/main.py
@@ -4,16 +4,12 @@ from encoder import TransformerEncoder
 N_BLOCKS = 6
 N_HEADS = 8
 D_MODEL = 128
-D_QK = 32
-D_V = 64
-FC_HIDDEN_DIM = 512
+D_FF = 512
 
 
 te = TransformerEncoder(
     n_blocks=N_BLOCKS,
     n_heads=N_HEADS,
     d_model=D_MODEL,
-    d_qk=D_QK,
-    d_v=D_V,
-    fc_hidden_dim=FC_HIDDEN_DIM
+    d_ff=D_FF
 )
