515c3c66563a7eca51cbbb5bd5dc23e96d6bb467
Mattia Massarenti <mattia.mas2003@gmail.com>
2026-09-24 10:11:34 +0200

    add device

--- transformer/main.py
+++ transformer/main.py
@@ -1,5 +1,9 @@
-from encoder import TransformerEncoder
+import torch
+from .encoder import TransformerEncoder
 
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print('Device:', device)
 
 N_BLOCKS = 6
 N_HEADS = 8
@@ -12,4 +16,5 @@ te = TransformerEncoder(
     n_heads=N_HEADS,
     d_model=D_MODEL,
     d_ff=D_FF
-)
+).to(device)
+
