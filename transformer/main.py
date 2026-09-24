58aa50adf7a9ac8f0bc7c2138c235ca700358887
Mattia Massarenti <mattia.mas2003@gmail.com>
2026-09-24 10:22:40 +0200

    setup flexible params print and input tensor

--- transformer/main.py
+++ transformer/main.py
@@ -1,20 +1,25 @@
 import torch
-from .encoder import TransformerEncoder
+
+from encoder import TransformerEncoder
 
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print('Device:', device)
 
-N_BLOCKS = 6
-N_HEADS = 8
-D_MODEL = 128
-D_FF = 512
+params = {
+    'num_layers': 6,
+    'num_heads': 8,
+    'd_model': 128,
+    'd_ff': 2048
+}
 
+print('Encoder parameters:', params)
 
 te = TransformerEncoder(
-    n_blocks=N_BLOCKS,
-    n_heads=N_HEADS,
-    d_model=D_MODEL,
-    d_ff=D_FF
+    n_blocks=params['num_layers'],
+    n_heads=params['num_heads'],
+    d_model=params['d_model'],
+    d_ff=params['d_ff']
 ).to(device)
 
+input = torch.empty(params['d_model']).to(device)
